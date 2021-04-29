83101ccc625056beeddc2d4b15d11101c050a328
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2021-04-28 23:08:41 -0400

    project html being replaced

--- src/projects.js
+++ src/projects.js
@@ -1,13 +1,16 @@
 export let projectList = (function () {
     let projects = [];
+    console.log(projects.length);
 
     // cacheDOM
     // Assigning from homepage.js
     const newProjButton = document.querySelector('#newProjButton');
     const listContainer = document.querySelector('.listContainer');
+
     let projInput = document.getElementById('projInput');
     // Creating html for projects
     const listItemContainer = document.createElement('div');
+
     const listItem = document.createElement('div');
     const listDelete = document.createElement('div');
 
@@ -23,24 +26,48 @@ export let projectList = (function () {
     //bindEvents
     newProjButton.addEventListener('click', createProject);
 
-    // Able to create project object and have it show up on the list, but it just replaces the previous object
+    // Creating project object and adding to array
     function createProject() {
         let projInputValue = projInput.value;
 
+        //creating new project object - will make this a separate module eventually
         const project = (projectTitle) => {
             return { projectTitle }
         }
+        const newProj = project(projInputValue);
 
-        const testProject = project(projInputValue);
-        console.log(testProject.projectTitle)
 
-        listContainer.appendChild(listItemContainer);
-        listItemContainer.appendChild(listItem);
-        listItemContainer.appendChild(listDelete);
-        
 
-        listItem.textContent = testProject.projectTitle;
+        // adding new project to projects array
+        projects.push(newProj);
+        _render();
+
+
+
+
     }
+
+    /* function _render() {
+        let i;
+        for (i = 0; i < projects.length; i++) {
+            console.log(projects)
+            listItem.textContent = projects[i].projectTitle;
+            listContainer.appendChild(listItemContainer);
+            listItemContainer.appendChild(listItem);
+            listItemContainer.appendChild(listDelete);
+
+        }
+    } */
+
+    function _render(){
+        projects.forEach(function (_project) {
+            listItem.textContent = _project.projectTitle;
+            listContainer.appendChild(listItemContainer);
+            listItemContainer.appendChild(listItem);
+            listItemContainer.appendChild(listDelete);
+        })
+    }
+    
 
 
 })();
