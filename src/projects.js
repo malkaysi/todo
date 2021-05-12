52073de5a8097ceaada9d93fef8b32da46b68137
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2021-05-11 22:43:18 -0400

    able to delete projects

--- src/projects.js
+++ src/projects.js
@@ -2,13 +2,6 @@ export let projectList = (function () {
     let projects = [];
     // Eventually need to move this to a sort
     let projectIndex = 0;
-    // project index is the length of the array
-    // if there are 3, than the index is 2?
-    // if the second one is erased a new one is added, than 2 items will have the same project index
-    // need to figure out a way to resort the existing project indexes
-    // add the project index to the object itself
-    // the project index from the object itself can be pulled and resorted using a loop
-    // need to find a way to insert the project index value to the delete html
 
     // cacheDOM
     // Assigning from homepage.js
@@ -37,19 +30,17 @@ export let projectList = (function () {
         return newProj;
     }
 
-    function deleteProject(e, newProj) {
+    function deleteProject(e, projectIndex) {
         let clickedEle = e.target
         if (clickedEle.id == 'listDelete') {
-            let index = clickedEle.projectIndex;
+            let index = clickedEle.getAttribute('projectIndex')
             
             // Get parent node of target to remove from the listContainer
             let listItemContainer = clickedEle.parentNode
             listContainer.removeChild(listItemContainer);
-            console.log('before deletion: ')
-            console.log(projects)
-            // It's still in the array for now
+
             projects.splice(index, 1);
-            console.log(projects)
+            sortProjectIndex();            
         }
     }
 
@@ -81,12 +72,10 @@ export let projectList = (function () {
 
     function sortProjectIndex() {
         let i;
-
         for(i=0; i<projects.length; i++){
-            listItem.setAttribute('value', '${i}')
+            projects[i].projectIndex = i;
         }
     }
 
-    // need to get 
 
 })();
